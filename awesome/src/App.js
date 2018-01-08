0e8dd6d7768b2c4577777e4796d8bb145515d930
曹又心 <c_lisheng@163.com>
2018-01-08 12:21:43 +0800

    grid

--- awesome/src/App.js
+++ awesome/src/App.js
@@ -6,10 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <div className="App-header-wrapper v-mid row-1200">
+          <header className="App-header col-12">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+        </div>
+        <div className="App-divider"></div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
